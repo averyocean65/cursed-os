05a16604eb3ea42787b1b6f2a156ad19882feb6a
averyocean65 <averytoad65@gmail.com>
2023-06-29 16:32:18 +0200

    yea this looks ready for publishing

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -28,9 +28,20 @@ void _start(void) {
 
     // Fetch the first framebuffer.
     struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
-    clear_screen(framebuffer, 0x252525);
-    draw_string(framebuffer, "It's Limine Time", g_8x8_font, CHAR_SIZE_8x8, 50, 50);
 
+    // Title
+    char* title = "The CURSED OS";
+    draw_string_8x16(framebuffer, title, 0xFFFFFF, framebuffer->width/2 - (strlen(title)*8), 0);
+
+    // Quotes
+    draw_string_8x16(framebuffer, "nya meow mrrp :3\n-rooot :3", 0xF900AA, 50, 50);
+    draw_string_8x16(framebuffer, "HANDS OFF MY BBG\n-GangstaCat", 0x35CBC8, 400, 70);
+    draw_string_8x16(framebuffer, "I am the bbg in question\n-Loafie", 0xA74B1D, 486, 70+64);
+    draw_string_8x16(framebuffer, "Do you want a banana? Peel it down and go mmmmmmm\n-banana song, a cult classic", 0xE4CC22, 400, 200);
+    draw_string_8x16(framebuffer, "KILL YOURSELF\n-Avery/Winter", 0xCE3351, 172, 200);
+
+    clear_screen(framebuffer, 0x0);
+    
     // We're done, just hang...
     hcf();
 }
--- kernel/text/drawing.h
+++ kernel/text/drawing.h
@@ -6,7 +6,7 @@
 #include "fonts.h"
 #include "../graphics.h"
 
-void draw_char(struct limine_framebuffer *framebuffer, char character, unsigned char *font, int char_size, uint16_t x, uint16_t y) {
+void draw_char(struct limine_framebuffer *framebuffer, char character, unsigned char *font, int char_size, uint32_t color, uint16_t x, uint16_t y) {
     unsigned char *glyph;
 
     // Get the font glyph based on font and char_size
@@ -18,24 +18,42 @@ void draw_char(struct limine_framebuffer *framebuffer, char character, unsigned 
         for (col = 0; col < char_size; col++) {
             if ((glyph[row] >> (char_size - 1 - col)) & 0x01) {
                 // Set the pixel to white
-                draw_pixel(framebuffer, x + col, y + row, 0xFFFFFFFF);
+                draw_pixel(framebuffer, x + col, y + row, color);
             }
         }
     }
 }
 
-void draw_string(struct limine_framebuffer *framebuffer, const char *str, unsigned char *font, int char_size, uint16_t x, uint16_t y) {
+void draw_string(struct limine_framebuffer *framebuffer, const char *str, unsigned char *font, int char_size, int char_spacing, uint32_t color, uint16_t x, uint16_t y) {
     unsigned int strLength = strlen(str);
     unsigned int charWidth = char_size;
+    unsigned int charHeight = char_size;
+
+    uint16_t cx = x;
+    uint16_t cy = y;
 
     // Draw each character of the string
     for (unsigned int i = 0; i < strLength; i++) {
         char character = str[i];
-        draw_char(framebuffer, character, font, char_size, x, y);
+        if(character == '\n') {
+            cx = x;
+            cy += charHeight;
+            continue;
+        }
 
-        // Move to the next character position
-        x += charWidth;
+        draw_char(framebuffer, character, font, char_size, color, cx, cy);
+
+        // Move to the next character position with custom spacing
+        cx += charWidth + char_spacing;
     }
+}
+
+void draw_string_8x16(struct limine_framebuffer *framebuffer, const char *str, uint32_t color, uint16_t x, uint16_t y) {
+    draw_string(framebuffer, str, g_8x16_font, CHAR_SIZE_8x16, -8, color, x, y);
+}
+
+void draw_string_8x8(struct limine_framebuffer *framebuffer, const char *str, uint32_t color, uint16_t x, uint16_t y) {
+    draw_string(framebuffer, str, g_8x8_font, CHAR_SIZE_8x8, 0, color, x, y);
 }
 
 #endif
